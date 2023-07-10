c4cd75d417f23c6bd98f0fbefcf48cf16ed23471
simpson9241 <simpson9241@gmail.com>
2023-07-10 18:32:54 +0900

    [Setting] Testing Github Action with docstring

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,9 @@
+"""Module for running Web Server"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 async def api_test():
+    """Function for API Test"""
     return "Hello World"
