e8f2087f5d7292f00f4ab39e0237242f69efa7d5
simpson9241 <simpson9241@gmail.com>
2023-10-09 19:09:49 +0900

    [Feat] Added Archive API

--- src/routers/archive_router.py
+++ src/routers/archive_router.py
@@ -1,29 +1,40 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.orm import Session
+from db.connection import get_db
+from views import archive_view
 
 archive = APIRouter(
     prefix="/archive"
 )
 
 
+# 아카이브 저장을 위한 API
+@archive.post("/")
+async def create_archive(request: Request, db: Session = Depends(get_db)):
+    archive_data = await request.json()
+
+    res = archive_view.create_archive(archive_data=archive_data, db=db)
+    return res
+
+
 # 아카이브 조회를 위한 API
-@archive.get('/')
-async def get_archive():
-    return {'msg': '아카이브 조회 성공'}
+@archive.get("/")
+def get_user(archive_id:int, user_id: int, db: Session = Depends(get_db)):
+    res = archive_view.get_archive_by_id(archive_id=archive_id,user_id=user_id, db=db)
+    return res
 
 
-# 아카이브 생성을 위한 API
-@archive.post('/')
-async def create_archive():
-    return {'msg': '아카이브 생성 성공'}
+# # 사용자 업데이트를 위한 API
+# @user.patch("/{user_id}")
+# async def update_user(request: Request, user_id: int, db: Session = Depends(get_db)):
+#     update_data = await request.json()
+
+#     res = user_view.update_user_by_id(user_id=user_id, data=update_data, db=db)
+#     return res
 
 
-# 아카이브 업데이트를 위한 API
-@archive.patch('/')
-async def update_archive():
-    return {'msg': '아카이브 업데이트 성공'}
-
-
-# 아카이브 삭제를 위한 API
-@archive.delete('/')
-async def delete_archive():
-    return {'msg': '아카이브 삭제 성공'}
+# # 사용자 삭제를 위한 API
+# @user.delete("/{user_id}")
+# async def delete_user(user_id: int, db: Session = Depends(get_db)):
+#     res = user_view.delete_user(user_id=user_id, db=db)
+#     return res
