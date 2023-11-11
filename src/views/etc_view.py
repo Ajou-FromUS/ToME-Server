628ecc01738255a572ede64133384a32eacc7def
bigpie1367 <bigpie1367@gmail.com>
2023-11-11 19:57:03 +0900

    [Update] 코드 리팩토링 및 결과물 형식 통일

--- src/views/etc_view.py
+++ src/views/etc_view.py
@@ -1,13 +1,13 @@
 from PIL import Image
 from io import BytesIO
 from fastapi import Request
-from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
+# from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
 
 import numpy as np
 import requests
 
 # ImageNet으로 미리 훈련된 InceptionV3 모델 로드
-model = InceptionV3(weights='imagenet')
+# model = InceptionV3(weights='imagenet')
 
 
 def refresh_token(request):
@@ -27,15 +27,15 @@ def refresh_token(request):
 
 
 # 이미지 분류 함수 정의
-def classify_image_by_imagenet(image):
-    img = Image.open(BytesIO(image))
-    img = img.resize((299, 299))
-    img = np.array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
+# def classify_image_by_imagenet(image):
+#     img = Image.open(BytesIO(image))
+#     img = img.resize((299, 299))
+#     img = np.array(img)
+#     img = np.expand_dims(img, axis=0)
+#     img = preprocess_input(img)
 
-    predictions = model.predict(img)
-    decoded_predictions = decode_predictions(predictions, top=3)[0]
+#     predictions = model.predict(img)
+#     decoded_predictions = decode_predictions(predictions, top=3)[0]
 
-    results = [{"label": label, "description": description, "probability": probability} for (label, description, probability) in decoded_predictions]
-    return results
+#     results = [{"label": label, "description": description, "probability": probability} for (label, description, probability) in decoded_predictions]
+#     return results
--- src/views/user_view.py
+++ src/views/user_view.py
@@ -1,21 +1,24 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
-from fastapi import HTTPException, status
+from fastapi import status, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from datetime import datetime
 from db.models.user_model import User
 
+# 에러 처리를 위한 함수
+def handle_error(status_code, detail):
+    return JSONResponse(content={"status_code": status_code, "detail": detail}, status_code=status_code)
 
 # 사용자 생성을 위한 API
 def create_user(user_data: dict, db: Session, token: str):
     # 필수 항목 누락 체크
     required_fields = ['nickname']
     if not all(field in user_data for field in required_fields):
-        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                            detail="필수 항목 중 일부가 누락되었습니다")
+        return handle_error(status.HTTP_422_UNPROCESSABLE_ENTITY, "필수 항목 중 일부가 누락되었습니다")
 
     try:
         uid = token['uid']
-
         user = User(
             uid=uid,
             nickname=user_data['nickname'],
@@ -23,77 +26,83 @@ def create_user(user_data: dict, db: Session, token: str):
             modified_at=datetime.now(),
             last_connection=datetime.now()
         )
+        user_json = jsonable_encoder(user)
 
         db.add(user)
         db.commit()
         db.refresh(user)
 
-        return {
-            "status_code": status.HTTP_201_CREATED,
-            "detail": "사용자 생성 성공",
-            "data": user
-        }
+        return JSONResponse(
+            content={"status_code": status.HTTP_201_CREATED, "detail": "사용자 생성 성공", "data": user_json},
+            status_code=status.HTTP_201_CREATED
+        )
     except IntegrityError:
         db.rollback()
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="이미 존재하는 사용자입니다")
+        return handle_error(status.HTTP_400_BAD_REQUEST, "이미 존재하는 사용자입니다")
     except Exception:
         db.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="사용자 생성 중 서버에 오류가 발생하였습니다")
+        return handle_error(status.HTTP_500_INTERNAL_SERVER_ERROR, "사용자 생성 중 오류가 발생하였습니다")
 
 
 # 사용자 조회를 위한 API
 def get_user_by_id(db: Session, token: str):
-    user = db.query(User).filter(User.uid == token['uid']).first()
+    try:
+        user = db.query(User).filter(User.uid == token['uid']).first()
+        user_json = jsonable_encoder(user)
 
-    if user is not None:
-        return {
-            "status_code": status.HTTP_200_OK,
-            "detail": "사용자 조회 성공",
-            "data": user
-        }
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="일치하는 사용자가 존재하지 않습니다")
+        if user:
+            return JSONResponse(
+                content={"status_code": status.HTTP_200_OK, "detail": "사용자 조회 성공", "data": user_json},
+                status_code=status.HTTP_200_OK
+            )
+        else:
+            return handle_error(status.HTTP_404_NOT_FOUND, "일치하는 사용자가 존재하지 않습니다")
+    except Exception:
+        return handle_error(status.HTTP_500_INTERNAL_SERVER_ERROR, "사용자 조회 중 오류가 발생하였습니다")
 
 
 # 사용자 업데이트를 위한 API
-def update_user_by_id(data: dict, db: Session, token: str):
-    user = get_user_by_id(db, token)['data']
-    valid = False
+def update_user_by_id(update_data: dict, db: Session, token: str):
+    try:
+        user = get_user_by_id(db, token).data
+        valid = False
 
-    # 일치하는 항목이 사용자 모델에 존재하는지 체크
-    for key, value in data.items():
-        if hasattr(user, key):
-            setattr(user, key, value)
-            valid = True
-        else:
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail="일치하는 항목이 존재하지 않습니다"
+        # 일치하는 항목이 사용자 모델에 존재하는지 체크
+        for key, value in update_data.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+                valid = True
+            else:
+                return handle_error(status.HTTP_422_UNPROCESSABLE_ENTITY, "일치하는 항목이 존재하지 않습니다")
+
+        # 존재할 경우 업데이트 수행
+        if valid:
+            db.commit()
+            db.refresh(user)
+
+            user_json = jsonable_encoder(user)
+
+            return JSONResponse(
+                content={"status_code": status.HTTP_200_OK, "detail": "사용자 업데이트 성공", "data": user_json},
+                status_code=status.HTTP_200_OK
             )
-
-    # 존재할 경우 업데이트 수행
-    if valid is True:
-        db.commit()
-        db.refresh(user)
-
-        return {
-            "status_code": status.HTTP_200_OK,
-            "detail": "사용자 업데이트 성공",
-            "data": user
-        }
+    except Exception:
+        db.rollback()
+        return handle_error(status.HTTP_500_INTERNAL_SERVER_ERROR, "사용자 업데이트 중 오류가 발생하였습니다")
 
 
 # 사용자 삭제를 위한 API
 def delete_user(db: Session, token: str):
-    user = get_user_by_id(db, token)['data']
+    try:
+        user = get_user_by_id(db, token).data
 
-    db.delete(user)
-    db.commit()
+        db.delete(user)
+        db.commit()
 
-    return {
-        "status_code": status.HTTP_200_OK,
-        "detail": "사용자 삭제 성공",
-    }
+        return JSONResponse(
+            content={"status_code": status.HTTP_200_OK, "detail": "사용자 삭제 성공"},
+            status_code=status.HTTP_200_OK
+        )
+    except Exception:
+        db.rollback()
+        return handle_error(status.HTTP_500_INTERNAL_SERVER_ERROR, "사용자 삭제 중 오류가 발생하였습니다")
